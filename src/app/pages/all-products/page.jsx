da0c3e760d498ec3838e94eabb84f4ca90119d01
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-09 12:52:28 +0500

    post api integrated

---
+++ src/app/pages/all-products/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+const page = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/api/v1/products');
+                setProducts(response.data.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    return (
+        <>
+            <div className="container mx-auto">
+                <h1 className="text-4xl font-bold my-8">Product List</h1>
+                <ul>
+                    {products.map((product) => (
+                        <li key={product._id} className="my-4 p-4 border rounded">
+                            <h2 className="text-2xl font-bold">{product.name}</h2>
+                            <p className="text-lg">Price: ${product.price}</p>
+                            <p className="text-lg">Company: {product.company}</p>
+                            <p className="text-lg">Color: {product.color}</p>
+                            <p className="text-lg">Category: {product.category}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default page
