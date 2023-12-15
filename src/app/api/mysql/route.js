66fa6c8c313993bab6b84dae68003f9d9dec9df9
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-15 15:12:44 +0500

    post api with my sql

--- src/app/api/mysql/route.js
+++ src/app/api/mysql/route.js
@@ -19,3 +19,27 @@ export async function GET(request) {
         return NextResponse.json(error);
     }
 }
+
+// post
+export async function POST(request) {
+    try {
+        const payload = await request.json()
+        const { title, description, status } = await payload;
+
+        const connection = await pool.getConnection();
+        const [result] = await connection.query(
+            'INSERT INTO about_us (title, description, status) VALUES (?, ?, ?)',
+            [title, description, status]
+        );
+        connection.release();
+
+        return NextResponse.json({
+            success: true,
+            message: `Data added successfully`,
+            insertedId: result.insertId,
+        });
+    } catch (error) {
+        console.error('Error adding data:', error.message);
+        return NextResponse.json({ success: false, message: error.message });
+    }
+}
