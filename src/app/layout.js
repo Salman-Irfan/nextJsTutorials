f99c29645ac8c9b886afa618eabb06ea9ae71fa3
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-15 12:27:25 +0500

    next js with my sql

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Providers } from '@/redux/providers'
 import { Inter } from 'next/font/google'
-// import './globals.css'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ src/components/Alert.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Alert = () => {
+    return (
+        <>
+            <div className="alert w-50 align-self-center alert-success alert-dismissible">
+                Data Added Successfully
+            </div>
+        </>
+    )
+}
+
+export default Alert
